413a67c94c5c82f47781ab43f026681343f7ac84
Kafka993 <68866021+Kafka993@users.noreply.github.com>
2020-08-13 22:22:30 -0300

    Update init.js

--- init.js
+++ init.js
@@ -1,29 +1,28 @@
-const LIBROS_URL = "https://github.com/Kafka993/oiraiD/blob/master/librosj.json";
+const LIBROS_URL = "https://github.com/Kafka993/oiraiD/librosj.json";
 
 
-var getJSONData = function(url){
-    var result = {};
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
+var getJSONData = function (url) {
+  var result = {};
+  
+  return fetch(url)
+      .then(response => {
+          if (response.ok) {
+              return response.json();
+              
+          } else {
+              throw Error(response.statusText); 
+          }
+      })
+
+      .then(function (response) {
           result.status = 'ok';
           result.data = response;
-        
           return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-       
-        return result;
-    });
-}
+      })
 
-
-      
+      .catch(function (error) {
+          result.status = 'error';
+          result.data = error;
+          return result;
+      });
+    };
