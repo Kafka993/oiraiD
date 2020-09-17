b10abcb6d4904ee29946350f131efa6dec78cebe
Kafka993 <68866021+Kafka993@users.noreply.github.com>
2020-09-17 12:39:09 -0300

    wikipedia 0.1v

--- Scripts/ver-libro.js
+++ Scripts/ver-libro.js
@@ -8,7 +8,14 @@ function showLibro(array){
   let htmlContentToAppend = ""
 for (let i = 0; i < libros.length; i++ ){
     let libro = array[i];
-    
+    let score = ""
+
+    for (let i = 1; i <= libro.Estrellas; i++) {
+        score += `<i class="fa fa-star checked"></i> `;
+    }
+    for (let i = libro.Estrellas + 1; i <= 5; i++) {
+        score += `<i class="fa fa-star"></i> `;
+    }
 
 if (libro.id == verLibro.libroId) { 
     
@@ -24,14 +31,20 @@ if (libro.id == verLibro.libroId) {
             <div class="col">
                     <h4 class="mb-3">` + libro.titulo +`</h4> <br> <br>
                     <h6 class="mb-2"> Autor: ${libro.Autor} <h6> 
-                    <h6 class="mb-2">` + "Genero: " + " " + libro.Genero   +" <br> <br> "+ "Páginas: " + libro.paginas + ` </h6> <br>
+                    <h6 class="mb-2">` + "Genero: " + " " + libro.Genero   +" <br> <br> "+ "Páginas: " + libro.paginas + ` </h6> <br> 
                     <h6 class="mb-2">` + libro.Estrellas  + ` Estrellas </h6> <br>
                     <h6 class="mb-2">` + libro.editorial  + `</h6> <br> <br>
-                    
+                    <p class="mb-2"> Para mas información sobre el libro y autor:</p><br>
+                    <a href="https://es.wikipedia.org/wiki/${libro.Autor.toLowerCase()}" class="btn btn-outline-success btn-sm"><i class="fa fa-wikipedia-w"> ${libro.Autor}</i></a>
+                    <a href="https://es.wikipedia.org/wiki/${libro.titulo.toLowerCase()}" class="btn btn-outline-success btn-sm"><i class="fa fa-wikipedia-w"> ${libro.titulo}</i></a>
+                    <div style="text-align: right;">` + score + `</div><br><hr>
                     </div></div></div>
 
                     `
 
+
+
+                    
 }
 } 
 
